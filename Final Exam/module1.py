030e274a6afa66b51a514ac3cb028ccc7926d782
ParthBS <parth.satodiya@gmail.com>
2017-04-28 11:50:01 +0530

    Update module1.py

--- Final Exam/module1.py
+++ Final Exam/module1.py
@@ -1,16 +1,16 @@
-import simplejson as json
-from nltk.tokenize import word_tokenize
-import unicodedata
-from nltk.stem import WordNetLemmatizer
-from collections import Counter
 import os
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import simplejson as json
+import unicodedata
+from collections import Counter
 
 lemmatizer = WordNetLemmatizer()
 
-def data_files(directory name):
+def dataFiles(directory_name):
     inp_files = []  # List which will store all of the full filepaths.
 
-    for d_path, directories, files in os.walk(directory name):
+    for d_path, directories, files in os.walk(directory_name):
         for name in files:
             # Join the two strings in order to form the full filepath.
             f_path = os.path.join(d_path, name)
@@ -18,8 +18,8 @@ def data_files(directory name):
 
     return inp_files
 
-def train():
-	inp_files = data_files("Candidate Profile Data")
+def train(profile):
+	inp_files = dataFiles("Candidate Profile Data")
 
 	for file in inp_files:
 		final=[]
@@ -28,29 +28,20 @@ def train():
 			json1_str = json1_file.read()
 
 			for json1_data in json.loads(json1_str):
-
-				# json1_data = json.loads(json1_str)[i]
-				# print type(json1_data['Additional-Info'])
 				if isinstance(json1_data['Additional-Info'], str):
 					temp = json1_data['Additional-Info']
 				else:
 					temp = unicodedata.normalize('NFKD', json1_data['Additional-Info']).encode('ascii','ignore')
-
-				# print temp
 
 				lexicon = []
 
 				all_words = word_tokenize(temp)
 				lexicon += list(all_words)
 
-				# print lexicon
-
 				temp=''
 				li=[]
-				# print lexicon
 				for i in lexicon:
-					# print i
-					if i==',' or i=='.' or i=='\n' or i=='*' or i=='/' or i=='&' or i=='=========':
+					if i==',' or i=='.' or i=='\n' or i=='*' or i=='/' or i=='&':
 						li.append(temp)
 						temp=''
 					elif i==':' or i=='=':
@@ -68,56 +59,60 @@ def train():
 			final = [i.lower() for i in final]
 			profile[file]=Counter(final)
 
-profile={}
 
-train()
+def test(test_file, profile):
+	json_file = open(test_file)
+	json_str = json_file.read()
 
-test_file = raw_input("Enter your json formatted resume file path: ")
+	for json_data in json.loads(json_str):
 
-json_file = open(test_file)
-json_str = json_file.read()
-
-
-for json_data in json.loads(json_str):
-
-	if isinstance(json_data['Additional-Info'], str):
-		temp = json_data['Additional-Info']
-	else:
-		temp = unicodedata.normalize('NFKD', json_data['Additional-Info']).encode('ascii','ignore')
-
-	lexicon = []
-
-	all_words = word_tokenize(temp)
-	lexicon += list(all_words)
-
-	temp=''
-	li=[]
-	for i in lexicon:
-		if i==',' or i=='.' or i=='\n' or i=='*' or i=='/' or i=='&' or i=='=========':
-			li.append(temp)
-			temp=''
-		elif i==':' or i=='=':
-			temp=''
+		if isinstance(json_data['Additional-Info'], str):
+			temp = json_data['Additional-Info']
 		else:
-			temp=temp+i
+			temp = unicodedata.normalize('NFKD', json_data['Additional-Info']).encode('ascii','ignore')
 
-	if li==[]:
-		lexicon = [lemmatizer.lemmatize(j) for j in lexicon]
-	else:
-		lexicon = [lemmatizer.lemmatize(j) for j in li]
+		lex = []
+
+		all_words = word_tokenize(temp)
+		lex += list(all_words)
+
+		temp=''
+		li=[]
+		for i in lex:
+			if i==',' or i=='.' or i=='\n' or i=='*' or i=='/' or i=='&':
+				li.append(temp)
+				temp=''
+			elif i==':' or i=='=':
+				temp=''
+			else:
+				temp=temp+i
+
+		if li==[]:
+			lex = [lemmatizer.lemmatize(j) for j in lex]
+		else:
+			lex = [lemmatizer.lemmatize(j) for j in li]
 
 
-ans=[]
-names=[]
-for i in profile:
-	cnt=Counter(profile[i])
-	ans.append([0,i])
-	for j in lexicon:
-		for k in profile[i]:
-			if j.lower()==k:
-				ans[-1][0]+=cnt[k]
+	ans=[]
+	names=[]
 
-ans.sort(key=lambda x:x[0], reverse=True)
+	for i in profile:
+		cnt=Counter(profile[i])
+		ans.append([0,i])
+		for j in lex:
+			for k in profile[i]:
+				if j.lower()==k:
+					ans[-1][0]+=cnt[k]
+
+	ans.sort(key=lambda x:x[0], reverse=True)
+	return ans
+
+test_file = raw_input("Enter Test File Path: ")
+
+profile={}
+train(profile)
+
+ans = test(test_file,profile)
 
 print 
 print 'Career Field : ', ans[0][1]
